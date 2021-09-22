b075a47f2b2a27954cce28f50654a9d3576bb315
Lancelot <xinshengdou674@gmail.com>
2021-09-22 20:02:38 +0800

    线性回归的简洁实现

--- d2l.py
+++ d2l.py
@@ -71,7 +71,7 @@ def load_data_fashion_mnist(batch_size, resize=None):
 def accuracy(y_hat, y):
     """ 计算预测正确的数量 """
     if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
-        y_hat = y_hat.argmax(axis=1)    # 类似于sum中的参数keepdim
+        y_hat = y_hat.argmax(axis=1)  # 类似于sum中的参数keepdim
     cmp = y_hat.type(y.dtype) == y
     return float(cmp.type(y.dtype).sum())
 
@@ -99,3 +99,39 @@ def evaluate_accuracy(net, data_iter):
     for X, y in data_iter:
         metric.add(accuracy(net(X), y), y.numel())
     return metric[0] / metric[1]
+
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    """ 训练一个模型的迭代周期 """
+    # 设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        lo = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用Pytorch内置的优化器和损失函数
+            updater.zero_grad()
+            lo.backward()
+            updater.step()
+            metric.add(float(lo) * len(y), accuracy(y_hat, y), y.size().numel())
+        else:
+            # 使用定制的优化器和损失函数
+            lo.sum().backward()
+            updater(X.shape[0])
+            metric.add(float(lo.sum()), accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练准确率
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+def train_ch3(net, train_iter, test_iter, loss, num_epoch3, updater):
+    """ 训练模型 """
+    for epoch in range(num_epoch3):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+    train_loss, train_acc = train_metrics
+    assert train_loss < 0.5, train_loss
+    assert train_acc <= 1 and train_acc > 0.7, train_acc
+    assert test_acc <= 1 and test_acc > 0.7, test_acc
